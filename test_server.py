ac7e0027d46ae7357ef8d8411db865c2610c5a67
Gaurav <gaurav.ssnl@gmail.com>
2020-08-02 02:58:00 +0530

    RServer test code in Python langauge added

---
+++ test_server.py
@@ -0,0 +1,25 @@
+import socket
+
+def test():
+    s = socket.socket()
+    server_address = ("127.0.0.1", 80)
+    s.connect(server_address)
+    print("Connected to the server")
+    request = "GET /test HTTP/1.1\r\n\r\n"
+    print("Sending request to server:\n", request)
+    s.sendall(request.encode())
+    print("Request sent successfullly")
+    print("Now read response from server")
+    response = b"";
+    while True:
+        data = s.recv(1024)
+        if data:
+            response += data
+            # if data==b"": break
+        else:
+            break
+    print("Response received from server completely:\n", response.decode())
+
+
+if __name__ == "__main__":
+    test()
